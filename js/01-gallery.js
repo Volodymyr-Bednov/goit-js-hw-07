425902c23ec4c988eeb130ad8d5eca743f1985d7
Volodymyr-Bednov <109081369+Volodymyr-Bednov@users.noreply.github.com>
2022-11-20 11:25:10 +0200

    js-hw-07-fin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector("div.gallery");
+galleryItems.map((value) => {
+  const { preview, original, description } = value;
+
+  const gItem = document.createElement("div");
+  gItem.classList.add("gallery__item");
+
+  const gLink = document.createElement("a");
+  gLink.classList.add("gallery__link");
+  gLink.href = original;
+
+  const gImage = document.createElement("img");
+  gImage.classList.add("gallery__image");
+  gImage.src = preview;
+  gImage.alt = description;
+  gImage.setAttribute("data-source", original);
+
+  gallery.append(gItem);
+  gItem.append(gLink);
+  gLink.append(gImage);
+});
+
+gallery.onclick = (ev) => {
+  ev.preventDefault();
+  if (ev.target.nodeName !== "IMG") return;
+  const imageOriginalURL = ev.target.dataset.source;
+
+  const dialogBLB = basicLightbox.create(
+    `
+  		<img width="1400" height="900" src="${imageOriginalURL}">
+  	`
+  );
+  dialogBLB.show(
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape") dialogBLB.close();
+    })
+  );
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector("ul.gallery");
+galleryItems.map((value) => {
+  const { preview, original, description } = value;
+
+  const gItem = document.createElement("li");
+
+  const gLink = document.createElement("a");
+  gLink.classList.add("gallery__item");
+  gLink.href = original;
+
+  const gImage = document.createElement("img");
+  gImage.classList.add("gallery__image");
+  gImage.src = preview;
+  gImage.alt = description;
+
+  gallery.append(gItem);
+  gItem.append(gLink);
+  gLink.append(gImage);
+});
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
